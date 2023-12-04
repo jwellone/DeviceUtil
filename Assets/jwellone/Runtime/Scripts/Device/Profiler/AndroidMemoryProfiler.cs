4e3abbf92778a40b2b52d2dfaa1e58e1444fea94
YasuhikoUsui <usui_yasuhiko@jwellone.co.jp>
2023-12-04 20:59:56 +0900

    Memory.aar追加

--- Assets/jwellone/Runtime/Scripts/Device/Profiler/AndroidMemoryProfiler.cs
+++ Assets/jwellone/Runtime/Scripts/Device/Profiler/AndroidMemoryProfiler.cs
@@ -7,61 +7,22 @@ namespace jwellone
 {
     public sealed class AndroidMemoryProfiler : IMemoryProfiler
     {
-        readonly int[] _ids = new int[1];
-        readonly AndroidJavaObject? _activeManager;
-        readonly AndroidJavaObject? _memoryInfo;
+
+        readonly AndroidJavaObject? _plugin;
 
         internal AndroidMemoryProfiler()
         {
-            using var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            using var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-            using var application = activity.Call<AndroidJavaObject>("getApplication");
-            using var context = application.Call<AndroidJavaObject>("getApplicationContext");
-            using var service = context.GetStatic<AndroidJavaObject>("ACTIVITY_SERVICE");
-            using var processClass = new AndroidJavaClass("android.os.Process");
-            _activeManager = context.Call<AndroidJavaObject>("getSystemService", service);
-            _ids[0] = processClass.CallStatic<int>("myPid");
-            _memoryInfo = new AndroidJavaObject("android.app.ActivityManager$MemoryInfo");
+            _plugin = new AndroidJavaObject($"com.jwellone.memory.Memory");
         }
 
         ~AndroidMemoryProfiler()
         {
-            _activeManager?.Dispose();
-            _memoryInfo?.Dispose();
+            _plugin?.Dispose();
         }
 
-        long IMemoryProfiler.usedMemorySize
-        {
-            get
-            {
-                var memoryInfo = _activeManager!.Call<AndroidJavaObject[]>("getProcessMemoryInfo", _ids);
-                long totalKB = 0;
-                foreach (var info in memoryInfo)
-                {
-                    totalKB += info.Call<int>("getTotalPss");
-                    info.Dispose();
-                }
-                return totalKB * 1024;
-            }
-        }
-
-        long IMemoryProfiler.availableMemorySize
-        {
-            get
-            {
-                _activeManager!.Call("getMemoryInfo", _memoryInfo);
-                return _memoryInfo!.Get<long>("availMem");
-            }
-        }
-
-        long IMemoryProfiler.totalMemorySize
-        {
-            get
-            {
-                _activeManager!.Call("getMemoryInfo", _memoryInfo);
-                return _memoryInfo!.Get<long>("totalMem");
-            }
-        }
+        long IMemoryProfiler.usedMemorySize => _plugin!.CallStatic<long>("getUsedMemorySize");
+        long IMemoryProfiler.availableMemorySize => _plugin!.CallStatic<long>("getAvailableMemorySize");
+        long IMemoryProfiler.totalMemorySize => _plugin!.CallStatic<long>("getTotalMemorySize");
     }
 }
 #endif
